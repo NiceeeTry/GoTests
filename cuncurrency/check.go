e6a92284d035731e180298ea04ff073dca80b92f
Alikhan Aitbayev <y>
2023-12-22 17:16:52 +0600

    cuncurrency

---
+++ cuncurrency/check.go
@@ -0,0 +1,24 @@
+package cuncurrency
+
+type WebsiteCheker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteCheker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+	return results
+}
